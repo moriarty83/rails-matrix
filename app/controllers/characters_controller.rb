91b03827a23b23e7f5332a0d27f0f47e9a032871
moriarty83 <moriarty83@gmail.com>
2021-12-11 16:06:23 -0500

    Index and Show Route

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,12 @@
 class CharactersController < ApplicationController
     def index
-        render json: Character.all
+        characters = Character.all
+        render json: {status: 200, characters: characters}
     end
 
     def show
-    
+        character = Character.find(params[:id])
+        render json: {status: 200, character: character}
     end
 
 end
