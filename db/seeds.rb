49e2ebc74bb0ad64914137bae68c5167ad132c54
moriarty83 <moriarty83@gmail.com>
2021-12-11 12:21:52 -0500

    Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of The Matrix franchise, created by the Wachowskis. He was portrayed as a cybercriminal and computer programmer by Keanu Reeves in the films, as well as having a cameo in The Animatrix short film Kid's Story." },
+	{ name: "Trinity", description: "Morpheus (/ˈmɔːrfiəs/) is a fictional character in The Matrix franchise.[2] He is portrayed by Laurence Fishburne in the first three films, and in the video game The Matrix: Path of Neo, where his original actor was the only one to reprise his character's voice." },
+	{ name: "Morpheus", description: "Trinity is a fictional character in the Matrix franchise.[1] She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale." },
+	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix franchise. He was portrayed by Hugo Weaving in the first trilogy of films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
 ])
